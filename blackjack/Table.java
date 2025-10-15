9204a737352386a74a0c037730c2898c1792046b
jpcode <107447587+jjpkeyboard@users.noreply.github.com>
2025-10-15 14:26:08 -0700

    added bin class files

---
+++ blackjack/Table.java
@@ -0,0 +1,11 @@
+package blackjack;
+public class Table {
+
+    private String dealerName = "Horatio";
+    private Player player; 
+    private Shoe shoe;
+
+    public Table() {
+        
+    }
+}
