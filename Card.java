f72290ef63167427b8b801a453e1c5ecf67bd93a
Tmanhertzog <tmanhertzog@gmail.com>
2025-10-15 13:07:18 -0700

    added compareTo

--- Card.java
+++ Card.java
@@ -1,11 +1,15 @@
-public class Card {
+import java.util.Comparator;
+
+public class Card implements Comparable{
 
     private String suit;
     private int value;
+    private String face;
 
-    public Card(String type, int val) {
+    public Card(String type, int val, String cardFace) {
         suit = type;
         value = val;
+        face = cardFace;
     }
 
     public String getSuit() {
@@ -15,4 +19,32 @@ public class Card {
     public int getVal() {
         return value;
     }
+
+    public String getFace() {
+        return face;
+    }
+
+    @Override
+    public int compareTo(Object obj) {
+        Card other = (Card)obj;
+        
+        int suitCompare = suit.compareToIgnoreCase(other.suit);
+        if (suitCompare != 0) return suitCompare;
+
+        // Compares the face cards
+        double thisRank = getSortValue();
+        double otherRank = other.getSortValue();
+
+        return Double.compare(thisRank, otherRank);
+    }
+
+    private double getSortValue() {
+        if (face == null) return value; // regular numbers
+        switch (face.toLowerCase()) {
+            case "jack": return 10.1;
+            case "queen": return 10.2;
+            case "king": return 10.3;
+            default: return value; // fallback
+    }
+}
 }
