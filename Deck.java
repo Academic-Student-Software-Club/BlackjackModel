0d98bc5628da6e56d1720c0de8463c2bd5be0915
jpcode <107447587+jjpkeyboard@users.noreply.github.com>
2025-10-14 21:41:43 -0700

    now deck creates more values other than 1, 2, 3, 4

--- Deck.java
+++ Deck.java
@@ -12,7 +12,7 @@ public class Deck {
 
         String suits[] = {"spade", "heart", "club", "diamond"};
         for (int i = 0; i < 52; i += 1) {
-            Card newCard = new Card(suits[i%4], i%4+1);
+            Card newCard = new Card(suits[i%4], i+1);
             deck.add(newCard);
         }
     }
--- Hand.java
+++ Hand.java
@@ -2,14 +2,21 @@ import java.util.ArrayList;
 
 public class Hand {
     
+    private ArrayList<Card> singleHand;
     //list of lists, when having multiple hands with splitting
-    private ArrayList<ArrayList<Card>> playerCards;
+    private ArrayList<ArrayList<Card>> handList;
 
     public Hand() {
-        playerCards = new ArrayList<ArrayList<Card>>();
+        singleHand = new ArrayList<Card>();
+        handList.add(singleHand);
     }
 
     public void addCard(Card newCard) {
-        playerCards.add(newCard);
+        singleHand.add(newCard);
+    }
+
+    //returns both hands
+    public ArrayList(splitHand() {
+
     }
 }
